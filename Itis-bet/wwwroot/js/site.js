d5c5d88793a4ec0077706b8c28a01fc5a721aa03
DanilArkhipov <danilarkhipov00@gmail.com>
2021-05-14 20:22:51 +0300

    Implemented header layout

--- Itis-bet/wwwroot/js/site.js
+++ Itis-bet/wwwroot/js/site.js
@@ -2,4 +2,11 @@
     $('.select').select2({
         placeholder:"asds",
     });
+
 });
+function coloriseSignInIcon(iconClass, textClass) {
+    $('#sign-in-icon').removeClass(textClass);
+    $('#sign-in-text').removeClass(iconClass);
+    $('#sign-in-icon').addClass(iconClass);
+    $('#sign-in-text').addClass(textClass);
+}
