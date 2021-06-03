113c89d42981d69d0432ed71bac324d61b2c5930
DanilArkhipov <danilarkhipov00@gmail.com>
2021-06-03 11:16:03 +0300

    added admin page

---
+++ Itis-bet/wwwroot/js/admin.js
@@ -0,0 +1,19 @@
+﻿function toggleMenu() {
+    let logo = $('#logo');
+    let menu = $('#menu');
+    let content = $('#content');
+    let className = 'toggled'
+    console.log(logo)
+    console.log(menu)
+    console.log(className)
+    if (logo.hasClass(className) && menu.hasClass(className) && content.hasClass(className)) {
+        logo.removeClass(className);
+        menu.removeClass(className);
+        content.removeClass(className);
+    }
+    else {
+        logo.addClass(className);
+        menu.addClass(className);
+        content.addClass(className);
+    }
+} 
