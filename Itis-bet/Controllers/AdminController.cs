8281cf71b30075cf461712e74341b35068386060
DanilArkhipov <danilarkhipov00@gmail.com>
2021-06-05 00:02:59 +0300

    Added Roles and users page in admin panel

--- Itis-bet/Controllers/AdminController.cs
+++ Itis-bet/Controllers/AdminController.cs
@@ -11,25 +11,33 @@ using Infrastructure;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Itis_bet.Controllers
 {
-  
+    [Authorize(Policy = "HasAccessToAdminPanel")]
     public class AdminController : Controller
     {
         private readonly Database _db;
         private IWebHostEnvironment _appEnvironment;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public AdminController(Database db, IWebHostEnvironment appEnvironment)
+        public AdminController(Database db, IWebHostEnvironment appEnvironment, SignInManager<User> signInManager, UserManager<User> manager)
         {
             _db = db;
             _appEnvironment = appEnvironment;
+            _signInManager = signInManager;
+            _userManager = manager;
         }
             
 
         [HttpGet]
         public IActionResult Index() =>
             View();
+        [Authorize(Policy = "Editor")]
         [HttpGet]
         public IActionResult BlogPosts()
         {
@@ -66,7 +74,7 @@ namespace Itis_bet.Controllers
             }
             return PartialView(enPosts);
         }
-
+        [Authorize(Policy = "Editor")]
         [HttpGet]
         public IActionResult CreateBlogPost()
         {
@@ -76,6 +84,7 @@ namespace Itis_bet.Controllers
             };
             return View(model);
         }
+        [Authorize(Policy = "Editor")]
         [HttpPost]
         public async Task<IActionResult> CreateBlogPost(CreateBlogVm model)
         {
@@ -108,7 +117,7 @@ namespace Itis_bet.Controllers
             }
             return RedirectToAction("BlogPosts");
         }
-
+        [Authorize(Policy = "Editor")]
         [HttpGet]
         public IActionResult Comments(string articleId)
         {
@@ -137,6 +146,61 @@ namespace Itis_bet.Controllers
             _db.SaveChanges();
             return RedirectToAction("Comments");
  
+        }
+        [Authorize(Policy = "Admin")]
+        [HttpGet]
+        public IActionResult Users(string userId)
+        {
+            var model = _db.Users
+                .Select(c => c);
+            if (userId != null)
+            {
+                Guid guid;
+
+                if (Guid.TryParse(userId, out guid))
+                    model = model.Where(c => c.Id == guid);
+
+            }
+
+            return View(model.AsEnumerable());
+        }
+        [HttpGet]
+        public IActionResult ViewUser(Guid userId)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.Id == userId);
+            return View("User",user);
+        }
+        [Authorize(Policy = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> SetClaim(Guid userId,string value)
+        {
+            var t = User.Claims;
+            var user = _db.Users.FirstOrDefault(u => u.Id == userId);
+            _db.UserClaims.Add(new IdentityUserClaim<Guid>()
+            {
+                ClaimType = ClaimTypes.Role,
+                ClaimValue = value,
+                UserId = userId,
+            });
+            await _db.SaveChangesAsync();
+            await _userManager.UpdateSecurityStampAsync(user);
+            return RedirectToAction("Users");
+        }
+        [Authorize(Policy = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> RemoveClaim(Guid userId, string value)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.Id == userId);
+            var claim = _db.UserClaims.Where(x => x.UserId == userId && x.ClaimValue == value).FirstOrDefault();
+            if (claim != null)
+            {
+                _db.UserClaims.Remove(claim);
+                await _db.SaveChangesAsync();
+            }
+            await _userManager.UpdateSecurityStampAsync(user);
+
+
+            return RedirectToAction("Users");
         }
 
     }
